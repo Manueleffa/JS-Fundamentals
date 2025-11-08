c830aff73761b6f31491ebfbabf5c3351b7e192d
Emmanuel Effa <manueleffa@gmail.com>
2025-11-08 21:24:24 +0100

    added the multilanguage script file

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+const firstLine = "C is fun";
+const secondLine = "Python is cool";
+const thirdtLine = "Javascript is amazing";
+
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdtLine);
