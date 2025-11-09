46d2aac1bed8c0f826cb9635a0a17bff6c8906e2
Emmanuel Effa <manueleffa@gmail.com>
2025-11-09 09:52:56 +0100

    added the arguments file

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const argument = process.argv.length;
+
+if (argument === 2){
+    console.log("No argument");
+} else if (argument === 3){
+    console.log("Argument found");
+} else{
+    console.log("Arguments found");
+}
