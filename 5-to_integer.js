fbd4a1029389627f61cc2af344415db339588897
Emmanuel Effa <manueleffa@gmail.com>
2025-11-09 11:59:35 +0100

    added the Math.trunc method to override decimal figures

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const x = process.argv[2];
 const num = Number(x);
 
 if (!isNaN(num)) {
-    console.log(`My number: ${num}`);
+  console.log(`My number: ${Math.trunc(num)}`);
 } else {
-    console.log("Not a number");
+  console.log("Not a number");
 }
